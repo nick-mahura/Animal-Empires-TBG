c47b3af303f7a7c26c174659ec6dddfb05916329
Nick Mahura <nickmahura@Nicks-MacBook-Air.local>
2018-01-05 13:47:01 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,148 @@
+var statsList = [{cls: "population__lions", name: "Lions", val: 1000},
+  {cls: "population__wolves", name: "Wolves", val: 1000},
+  {cls: "population__foxes", name: "Foxes", val: 300},
+  {cls: "population__owls", name: "Owls", val: 100},
+  {cls: "population__bears", name: "Bears", val: 500},             
+  {cls: "population__monkeys", name: "Monkeys", val: 300},
+  {cls: "population__roaches", name: "Roaches", val: 10000}];
+var player__name = name;
+var racesList = ['Lions', 'Wolves', 'Foxes', 'Owls', 'Bears', 'Monkeys', 'Roaches'];
+
+function gameStartMenu() {
+  //Creating starting screen
+  var startingScreen = document.createElement('div');
+  startingScreen.className = 'starting-screen';
+  document.querySelector('.wrapper').appendChild(startingScreen);
+  
+  //Creating starting screen form
+  var startingScreenForm = document.createElement('form');
+  startingScreenForm.className = 'starting-screen__form';
+  document.querySelector('.starting-screen').appendChild(startingScreenForm);
+  
+  //Creating starting screen header
+  var startingScreenHeader = document.createElement('p');
+  startingScreenHeader.className = 'starting-screen__name';
+  startingScreenHeader.innerHTML = "Enter your name:";
+  document.querySelector('.starting-screen__form').appendChild(startingScreenHeader);
+  
+  //Creating starting screen name input
+  var startingScreenNameInput = document.createElement('input');
+  startingScreenNameInput.setAttribute('type','text');
+  startingScreenNameInput.className = 'starting-screen__name-input';
+  document.querySelector('.starting-screen__form').appendChild(startingScreenNameInput);
+  
+  //Creating starting screen start button
+  var startingScreenStartButton = document.createElement('input');
+  startingScreenStartButton.setAttribute('type','button');
+  startingScreenStartButton.setAttribute('value','Start');
+  startingScreenStartButton.className = 'starting-screen__start-btn';
+  document.querySelector('.starting-screen__form').appendChild(startingScreenStartButton);
+  
+  function gameStart() {
+    createGame()
+    console.log(document.querySelectorAll('.stats__population')[0]);
+    changeLionsPopulation(3000);
+    changeWolvesPopulation(4000);
+    changeFoxesPopulation(300);
+    changeOwlsPopulation(50);
+    console.log("game started");
+  }
+  
+    document.querySelector('.starting-screen__start-btn').onclick = function() {
+      console.log('game started');
+      playerName();
+      document.querySelector('.starting-screen').remove();
+      setTimeout(createGame, 500);
+  }
+}
+
+function createGame() {
+  //Creating gamewrapper
+  var gameWrapper = document.createElement('div');
+  gameWrapper.className = 'game__wrapper';
+  document.querySelector('.wrapper').appendChild(gameWrapper);
+  
+  //Creating game header
+  var gameHeader = document.createElement('h1');
+  gameHeader.className = 'game__header';
+  gameHeader.innerHTML = 'Hello ' + player__name + "!";
+  document.querySelector('.game__wrapper').appendChild(gameHeader);
+  
+  //Creating game header
+  var gameContent = document.createElement('section');
+  gameContent.className = 'game__content';
+  document.querySelector('.game__wrapper').appendChild(gameContent);
+  
+  //Creating game stats
+  var gameStats = document.createElement('aside');
+  gameStats.className = 'game__stats';
+  document.querySelector('.game__content').appendChild(gameStats);
+  
+  //Creating game stats list
+  var gameStatsList = document.createElement('ul');
+  gameStatsList.className = 'stats__list';
+  document.querySelector('.game__stats').appendChild(gameStatsList);
+  
+  //Creating game stats list items
+  statsList.map(function(obj) {
+    let li = document.createElement('li');
+    li.className = 'stats__population';
+    li.innerHTML = obj.name;
+    document.querySelector('.stats__list').appendChild(li);
+    let span = document.createElement('span');
+    span.classList = obj.cls;
+    span.innerHTML = " " + obj.val;
+    li.appendChild(span);
+  });
+  
+  var exit__btn = document.createElement('button');
+  exit__btn.className = 'exit__btn';
+  exit__btn.innerHTML = "EXIT";
+  gameWrapper.appendChild(exit__btn);
+  
+  document.querySelector('.exit__btn').onclick = function() {
+    console.log('exit');
+    gameWrapper.remove();
+    gameStartMenu();
+  }
+ 
+}
+
+
+
+gameStartMenu();
+
+
+
+
+
+function changeLionsPopulation(n) {
+  document.querySelector('.population__lions').innerHTML = n;
+}//
+
+function changeWolvesPopulation(n) {
+  document.querySelector('.population__wolves').innerHTML = n;
+}
+
+function changeFoxesPopulation(n) {
+  document.querySelector('.population__foxes').innerHTML = n;
+}
+
+function changeOwlsPopulation(n) {
+  document.querySelector('.population__owls').innerHTML = n;
+}
+
+function playerName() {
+  player__name = document.querySelector('.starting-screen__name-input').value;
+  
+  console.log(player__name);
+  document.querySelector('.starting-screen__name-input').value = "";
+}
+  
+  
+
+
+  
+  
+
+
